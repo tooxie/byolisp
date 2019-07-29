1cc8a50d279c9cbb81ad6a14efe6183c84e79e44
Alvaro Mourino <alvaro.mourino@spring-media.de>
2019-07-29 09:00:45 +0200

    Add repl.c

---
+++ src/repl.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* If we are compiling on Windows, compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+
+#ifdef __linux__
+#include <editline/history.h>
+#endif
+#endif
+
+int main(int argc, char** argv) {
+    /* Print version and exit information */
+    puts("Lispy version 0.0.0.0.1");
+    puts("Press Ctrl+c to exit\n");
+
+    /* In a never ending loop */
+    while(1) {
+        /* Print prompt and get input */
+        char* input = readline("lispy> ");
+
+        /* Read a line of user input of max size 2048 */
+        add_history(input);
+
+        /* Echo input back to user */
+        printf("> No, you are a %s!\n", input);
+
+        /* Free retrieved input */
+        free(input);
+    }
+
+    return 0;
+}
